061a0754ccc4ec862ae3ca002266d213e6d1bf73
Diego Rodolfo Inacio <rodolfo.diego@yahoo.com.br>
2024-03-07 14:21:32 -0300

    Desafio 4

---
+++ desafio04.py
@@ -0,0 +1,6 @@
+# 1° Ligue o interruptor 1 e deixe ligado por alguns minutos.
+# 2° Desligue o interruptor 1 e ligue o interruptor 2.
+# 3° Vá para a sala das lâmpadas.
+# 4° A Lâmpada que estiver quente é controlada pelo interruptor 1.
+# 6° A lâmpada que estiver acesa é a controlada pelo interruptor 2.
+# 7° A lâmpada fria é controlada pelo interruptor 3.
