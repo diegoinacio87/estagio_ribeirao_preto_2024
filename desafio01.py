5946b0398edf7a71df2a3d153799cf2bc8672e9c
Diego Rodolfo Inacio <rodolfo.diego@yahoo.com.br>
2024-03-07 13:47:33 -0300

    Primeiro desafio

---
+++ desafio01.py
@@ -0,0 +1,15 @@
+INDICE = 13
+SOMA = 0
+K = 0
+
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+print(SOMA)
+
+# O valor final da variável SOMA será 91. #
+# Explicação: #
+# O loop while é executado 13 vezes, pois INDICE é igual a 13. #
+# A cada iteração, K é incrementado em 1 e SOMA é incrementado em K. #
+# No final do loop, K será igual a 14 e SOMA será a soma de todos os números de 1 a 13, que é 91. #
